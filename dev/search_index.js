d6bb938ba39ce5cccb3ffcbf9697076f6605bbd7
Documenter.jl <documenter@juliadocs.github.io>
2021-06-15 23:00:44 +0000

    build based on 115dad5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Cleaner","category":"page"},{"location":"#Cleaner","page":"Home","title":"Cleaner","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Cleaner.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Cleaner]","category":"page"}]
+}
