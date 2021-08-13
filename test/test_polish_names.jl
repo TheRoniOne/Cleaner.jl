c31ae388c72041d75297a292905c9797faada00e
TheRoniOne <ronidani99@hotmail.com>
2021-08-12 19:30:40 -0500

    updated polish tests

--- test/test_polish_names.jl
+++ test/test_polish_names.jl
@@ -1,17 +1,17 @@
 using Test
 using Cleaner: polish_names!, generate_polished_names
 using DataFrames: DataFrame, rename!
-import Tables: columnnames
+using Tables: columnnames
 
 @testset "name polishing is working" begin
     testDF = DataFrame("  _aName with_loTsOfProblems" => [1,2,3], 
     "  _aName with_loTsOfProblems1" => [4,5,6], 
     "  _aName with_loTsOfProblems2" => [7,8,9])
 
-    @test Tables.columnnames(polish_names!(testDF, style="snake_case")) == Vector{Symbol}(
+    @test columnnames(polish_names!(testDF, style="snake_case")) == Vector{Symbol}(
         [:a_name_with_lo_ts_of_problems, :a_name_with_lo_ts_of_problems1, :a_name_with_lo_ts_of_problems2])
     
-    @test Tables.columnnames(polish_names!(testDF, style="camelCase")) == Vector{Symbol}(
+    @test columnnames(polish_names!(testDF, style="camelCase")) == Vector{Symbol}(
         [:aNameWithLoTsOfProblems, :aNameWithLoTsOfProblems1, :aNameWithLoTsOfProblems2])
 
     @test generate_polished_names(["  _aName with_loTsOfProblems", "  _aName with_loTsOfProblems", 
