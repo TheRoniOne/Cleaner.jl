5fb70fdcea25e5fe8920318262299bd2ca0e88e9
TheRoniOne <ronidani99@hotmail.com>
2021-08-18 01:59:15 -0500

    implemented CleanTable struct using Tables interface

---
+++ src/CleanTable.jl
@@ -0,0 +1,31 @@
+import Tables
+
+mutable struct CleanTable <: Tables.AbstractColumns
+    names::Vector{Symbol}
+    cols::Vector{AbstractVector}
+end
+
+_getvector(x::AbstractVector) = x
+_getvector(x) = collect(x)
+
+function CleanTable(x)
+    names = [Symbol(name) for name in Tables.columnnames(x)]
+    cols::Vector{AbstractVector} = AbstractVector[_getvector(Tables.getcolumn(x, name)) for name in names]
+
+    return CleanTable(names, cols)
+end
+
+Tables.istable(::Type{<:CleanTable}) = true
+
+names(ct::CleanTable) = getfield(ct, :names)
+cols(ct::CleanTable) = getfield(ct, :cols)
+
+Tables.schema(ct::CleanTable) = Tables.Schema(names(ct), [eltype(col) for col in cols(ct)])
+
+Tables.columnaccess(::Type{<:CleanTable}) = true
+Tables.columns(ct::CleanTable) = ct
+Tables.getcolumn(ct::CleanTable, i::Int) = getindex(cols(ct), i)
+Tables.getcolumn(ct::CleanTable, name::Symbol) = getindex(cols(ct), findfirst(isequal(name), names(ct)))
+Tables.columnnames(ct::CleanTable) = names(ct)
+
+Tables.materializer(ct::CleanTable) = CleanTable
