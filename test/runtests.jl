0422787953316a25ad51241f62c14e7c7fa9f481
TheRoniOne <ronidani99@hotmail.com>
2021-08-20 22:56:41 -0500

    added test sets for compact and delete_const

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using SafeTestsets
 
-@safetestset "test_polish_names.jl" begin include("test_polish_names.jl") end
 @safetestset "test_CleanTable.jl" begin include("test_CleanTable.jl") end
+@safetestset "test_polish_names.jl" begin include("test_polish_names.jl") end
+@safetestset "test_compact.jl" begin include("test_compact.jl") end
+@safetestset "test_delete_const.jl" begin include("test_delete_const.jl") end
