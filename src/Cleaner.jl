edb94b1096b7038e763666926666da505e200ffd
TheRoniOne <ronidani99@hotmail.com>
2021-08-20 22:46:43 -0500

    added compact.jl and delete.const includes and exports

--- src/Cleaner.jl
+++ src/Cleaner.jl
@@ -2,9 +2,27 @@ module Cleaner
 
 include("CleanTable.jl")
 include("polish_names.jl")
+include("compact.jl")
+include("delete_const.jl")
 
+# CleanTable.jl exports
+export size
+
+# polish_names.jl exports
 export polish_names!
 export polish_names
 export generate_polished_names
+
+# compact.jl exports
+export compact_table!
+export compact_table
+export compact_columns!
+export compact_columns
+export compact_rows!
+export compact_rows
+
+# delete_const.jl exports
+export delete_const_columns!
+export delete_const_columns
 
 end
