68c5b7a50d60d165edd9cfb352bed8b01af4512c
TheRoniOne <ronidani99@hotmail.com>
2021-08-20 23:27:47 -0500

    added pass condition

--- test/test_compact.jl
+++ test/test_compact.jl
@@ -1,15 +1,17 @@
 using Test
-using Cleaner: CleanTable, compact_cols, compact_rows, compact_table
+using Cleaner: CleanTable, compact_columns, compact_rows, compact_table, size, names
 
 @testset "Compact functions working fine" begin
     testRM1 = CleanTable([:A, :B, :C], [[missing, missing, missing], [1, missing, 3], ["x", "", "z"]])
     testRM2 = CleanTable([:A, :B, :C], [["", missing, ""], [1, missing, 3], ["x", missing, "z"]])
 
-    @test compact_cols(testRM1)
-    @test compact_rows(testRM2)
-    @test compact_table(testRM1)
+    @test compact_columns(testRM1) |> size == (3, 2)
+    @test compact_rows(testRM2) |> size == (2, 3)
+    @test compact_table(testRM1) |> size == (3, 2)
 
-    @test compact_cols(testRM2, empty_values=[""])
-    @test compact_rows(testRM1, empty_values=[""])
-    @test compact_table(testRM2, empty_values=[""])
+    @test compact_columns(testRM2, empty_values=[""]) |> size == (3, 2)
+    @test compact_rows(testRM1, empty_values=[""]) |> size == (2, 3)
+    @test compact_table(testRM2, empty_values=[""]) |> size == (2, 2)
+
+    @test names(compact_rows(testRM2)) == [:B, :C]
 end
