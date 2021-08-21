01df4b173da3be6aa2646816fb891b713a60fc46
TheRoniOne <ronidani99@hotmail.com>
2021-08-20 21:04:18 -0500

    fixes on compact_columns! and dev compact_rows!

--- src/compact.jl
+++ src/compact.jl
@@ -7,25 +7,51 @@ function compact_table(table; empty_values::Vector=[])
 end
 
 function compact_columns!(table::CleanTable; empty_values::Vector=[])
-    filter!(x -> _is_not_empty_col(x, empty_values=empty_values), cols(table))
+    columns = cols(table)
+    ndel = 0
+
+    for i in 1:length(columns)
+        if _is_empty_col(columns[i - ndel], empty_values=empty_values)
+            deleteat!(columns, i - ndel)
+            deleteat!(names(table), i - ndel)
+            ndel += 1
+        end
+    end
 end
 
 function compact_columns(table; empty_values::Vector=[])
     return compact_columns!(CleanTable(table), empty_values=empty_values)
 end
 
-function _is_not_empty_col(col; empty_values::Vector=[])
+function _is_empty_col(col; empty_values::Vector=[])
     for el in skipmissing(col)
-        isempty(empty_values) && return true
+        isempty(empty_values) && return false
 
-        !in(el, empty_values) && return true
+        !in(el, empty_values) && return false
     end
     
-    return false
+    return true
 end
 
 function compact_rows!(table::CleanTable; empty_values::Vector=[])
+    columns = cols(table)
+    lcol = length(col)
+    row_state = replace!(Vector{Bool}(undef, lcol), true => false)
     
+    for col in columns
+        i = 1
+        while !row_state[i] && i <= lcol
+            if !ismissing(col[i]) && !in(col[i], empty_values)
+                row_state[i] = true
+            end
+            i += 1
+        end
+    end
+
+    to_delete = findall(!, row_state)
+    for col in columns
+        map(x -> deleteat!(col, x), to_delete)
+    end
 end
 
 function compact_rows(table; empty_values::Vector=[])
