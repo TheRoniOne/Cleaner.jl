772f0d1ceff9a69596cd8622e100ce308244179b
TheRoniOne <ronidani99@hotmail.com>
2021-08-20 22:44:36 -0500

    added tests for delete_const_columns

---
+++ test/test_delete_const.jl
@@ -0,0 +1,9 @@
+using Test
+using Cleaner: CleanTable, delete_const_columns
+
+@testset "Delete constant columns works as expected" begin
+    testCT = CleanTable([:A, :B, :C], [[1, 1, 1], [4, 5, 6], String["7", "8", "9"]])
+
+    @test delete_const_columns(testCT) |> size == (3, 2)
+    @test delete_const_columns(testCT) isa CleanTable
+end
