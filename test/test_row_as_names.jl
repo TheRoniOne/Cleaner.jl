57526995e772f36a51fad0e23bad531fe69d9477
TheRoniOne <ronidani99@hotmail.com>
2021-08-22 15:46:52 -0500

    fixed testCT rows

--- test/test_row_as_names.jl
+++ test/test_row_as_names.jl
@@ -2,7 +2,7 @@ using Test
 using Cleaner: CleanTable, row_as_names, reinfer_schema, names, size
 
 @testset "row_as_names is working as expected" begin
-    testCT = CleanTable([:A, :B, :C], [[1, 2, 3], [4, 5, 6], String["x", "y", "z"], String["7", "8", "9"]])
+    testCT = CleanTable([:A, :B, :C], [[1, 2, "x", 4], [5, 6, "y", 7], ["x", "y", "z", "a"]])
 
     @test names(row_as_names(testCT, 3)) == Symbol[:x, :y, :z]
     @test size(row_as_names(testCT, 3)) == (1, 3)
