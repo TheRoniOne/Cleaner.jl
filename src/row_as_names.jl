a6e3d3c407c1f795c322d3d6b97f9120eb42dc62
TheRoniOne <ronidani99@hotmail.com>
2021-08-22 17:21:03 -0500

    added docstrings

--- src/row_as_names.jl
+++ src/row_as_names.jl
@@ -1,3 +1,10 @@
+"""
+    row_as_names!(table::CleanTable, i::Int; remove::Bool=true)
+
+Renames the table using row i as new names and removes in-place all the rows above row i if remove=true.
+
+Default behavior is to remove rows above row i.
+"""
 function row_as_names!(table::CleanTable, i::Int; remove::Bool=true)
     columns = cols(table)
     new_names = [Symbol(col[i]) for col in columns]
@@ -16,6 +23,14 @@ function row_as_names!(table::CleanTable, i::Int; remove::Bool=true)
     return table
 end
 
-function row_as_names(table, i::Int)
+"""
+    row_as_names(table, i::Int; remove::Bool=false)
+
+Creates a CleanTable with copied columns and renames the table using row i as new names and removes 
+in-place all the rows above row i if remove=true.
+
+Default behavior is to remove rows above row i.
+"""
+function row_as_names(table, i::Int; remove::Bool=false)
     return row_as_names!(CleanTable(table), i)
 end
