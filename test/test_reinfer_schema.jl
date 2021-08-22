81315136882de07b6d17ec1f28ccbc14f23c8222
TheRoniOne <ronidani99@hotmail.com>
2021-08-22 16:41:41 -0500

    fixed expected result

--- test/test_reinfer_schema.jl
+++ test/test_reinfer_schema.jl
@@ -1,10 +1,11 @@
 using Test
+using Tables: schema, Schema
 using Cleaner: CleanTable, reinfer_schema
 
 @testset "reinfer_schema is working as expected" begin
     testCT = CleanTable([:A, :B, :C], [[1, 2, 3], Any[4, missing, "z"], Any["5", "6", "9"]])
     testCT2 = CleanTable([:A, :B, :C], [[1, 2, 3, 4], Any[5, missing, "z", 2.0], Any["6", "7", "8", "9"]])
     
-    @test reinfer_schema(testCT)
-    @test reinfer_schema(testCT2)
+    @test schema(reinfer_schema(testCT)) == Schema([:A, :B, :C], [Int, Union{Missing, String, Int}, String])
+    @test schema(reinfer_schema(testCT2)) == Schema([:A, :B, :C], [Int, Union{Missing, Real, String}, String])
 end
