4d4aabde301cbf9fc62ac54bda5e5029352f25c8
TheRoniOne <ronidani99@hotmail.com>
2021-08-22 16:40:40 -0500

    bug fix

--- src/reinfer_schema.jl
+++ src/reinfer_schema.jl
@@ -1,27 +1,31 @@
 function reinfer_schema!(table::CleanTable, max_types::Int=3)
     columns = cols(table)
-    for col in columns
-        types = Vector{DataType}()
-        number_types = Vector{DataType}()
+    for i in 1:length(columns)
+        col = columns[i]
+        type = eltype(col)
+        if type == Any
+            types = Vector{DataType}()
+            number_types = Vector{DataType}()
 
-        for T in unique(typeof.(col))
-            if T <: Number
-                push!(number_types, T)
-            else
-                push!(types, T)
+            for T in unique(typeof.(col))
+                if T <: Number
+                    push!(number_types, T)
+                else
+                    push!(types, T)
+                end
             end
-        end
 
-        if !isempty(number_types)
-            push!(types, reduce(Base.promote_typejoin, number_types))
-        end
+            if !isempty(number_types)
+                push!(types, reduce(Base.promote_typejoin, number_types))
+            end
 
-        if length(types) <= max_types
-            convert(Vector{Union{types...}}, col)
-        end        
-        
+            if length(types) <= max_types
+                columns[i] = convert(Vector{Union{types...}}, col)
+            end        
+            
+        end
     end
-    
+
     return table
 end
 
