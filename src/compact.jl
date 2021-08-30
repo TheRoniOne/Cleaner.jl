70d6b9bdf7ea75b1d8209d897e67238ea5ae1649
TheRoniOne <ronidani99@hotmail.com>
2021-08-30 00:29:01 -0500

    preparing multithreading

--- src/compact.jl
+++ src/compact.jl
@@ -31,11 +31,28 @@ function compact_columns!(table::CleanTable; empty_values::Vector=[])
     columns = cols(table)
     ndel = 0
 
-    for i in 1:length(columns)
-        if _is_empty_col(columns[i - ndel]; empty_values=empty_values)
-            deleteat!(columns, i - ndel)
-            deleteat!(names(table), i - ndel)
+    if Threads.nthreads() > 1 && length(columns) > 1 && length(columns[1]) >= 1_000_000
+        col_state = replace!(Vector{Bool}(undef, length(columns)), true => false)
+
+        Threads.@threads for i in 1:length(columns)
+            if _is_empty_col(columns[i]; empty_values=empty_values)
+                col_state[i] = true
+            end
+        end
+
+        to_delete = findall(col_state)
+        for j in to_delete
+            deleteat!(columns, j - ndel)
+            deleteat!(names(table), j - ndel)
             ndel += 1
+        end
+    else
+        for i in 1:length(columns)
+            if _is_empty_col(columns[i - ndel]; empty_values=empty_values)
+                deleteat!(columns, i - ndel)
+                deleteat!(names(table), i - ndel)
+                ndel += 1
+            end
         end
     end
 
@@ -73,21 +90,41 @@ function compact_rows!(table::CleanTable; empty_values::Vector=[])
     nrows = length(columns[1])
     row_state = replace!(Vector{Bool}(undef, nrows), true => false)
 
-    for col in columns
-        i = 1
+    if Threads.nthreads() > 1 && length(columns) > 1 && length(columns[1]) >= 1_000_000
+        Threads.@threads for col in columns
+            i = 1
 
-        while i <= nrows
-            if !row_state[i] && !ismissing(col[i]) && !in(col[i], empty_values)
-                row_state[i] = true
+            while i <= nrows
+                if !row_state[i] && !ismissing(col[i]) && !in(col[i], empty_values)
+                    row_state[i] = true
+                end
+                i += 1
             end
-            i += 1
         end
-    end
 
-    to_delete = findall(!, row_state)
-    for col in columns
-        for num in 0:(length(to_delete) - 1)
-            deleteat!(col, to_delete[num + 1] - num)
+        to_delete = findall(!, row_state)
+        Threads.@threads for col in columns
+            for num in 0:(length(to_delete) - 1)
+                deleteat!(col, to_delete[num + 1] - num)
+            end
+        end
+    else
+        for col in columns
+            i = 1
+
+            while i <= nrows
+                if !row_state[i] && !ismissing(col[i]) && !in(col[i], empty_values)
+                    row_state[i] = true
+                end
+                i += 1
+            end
+        end
+
+        to_delete = findall(!, row_state)
+        for col in columns
+            for num in 0:(length(to_delete) - 1)
+                deleteat!(col, to_delete[num + 1] - num)
+            end
         end
     end
 
