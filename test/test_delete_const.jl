5f158b85f86fdacd7d663003b5268b9a698165f9
TheRoniOne <ronidani99@hotmail.com>
2021-08-30 02:44:36 -0500

    added tests for multithreaded version

--- test/test_delete_const.jl
+++ test/test_delete_const.jl
@@ -1,9 +1,16 @@
 using Test
-using Cleaner: CleanTable, delete_const_columns, size
+using Cleaner: CleanTable, delete_const_columns, size, names
 
 @testset "Delete constant columns works as expected" begin
     testCT = CleanTable([:A, :B, :C], [[1, 1, 1], [4, 5, 6], String["7", "8", "9"]])
 
-    @test delete_const_columns(testCT) |> size == (3, 2)
+    @test size(delete_const_columns(testCT)) == (3, 2)
+    @test names(delete_const_columns(testCT)) == [:B, :C]
     @test delete_const_columns(testCT) isa CleanTable
+
+    if Threads.nthreads() > 1
+        testCT2 = CleanTable([:A, :B], [repeat([1], 1_000_000), collect(1:1_000_000)])
+        @test size(delete_const_columns(testCT2)) == (1_000_000, 1)
+        @test names(delete_const_columns(testCT2)) == [:B]
+    end
 end
