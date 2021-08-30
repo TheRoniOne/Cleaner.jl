7bad4f3487e25587e80b029171c173aef69ab7a3
TheRoniOne <ronidani99@hotmail.com>
2021-08-30 03:10:57 -0500

    dev multithreading version

--- src/reinfer_schema.jl
+++ src/reinfer_schema.jl
@@ -10,27 +10,54 @@ If not possible, leaves the column as-is.
 """
 function reinfer_schema!(table::CleanTable; max_types::Int=3)
     columns = cols(table)
-    for i in 1:length(columns)
-        col = columns[i]
-        type = eltype(col)
-        if type == Any
-            types = Vector{DataType}()
-            number_types = Vector{DataType}()
+    if Threads.nthreads() > 1 && length(columns) > 1 && length(columns[1]) >= 1_000_000
+        Threads.@threads for i in 1:length(columns)
+            col = columns[i]
+            type = eltype(col)
+            if type == Any
+                types = Vector{DataType}()
+                number_types = Vector{DataType}()
 
-            for T in unique(typeof.(col))
-                if T <: Number
-                    push!(number_types, T)
-                else
-                    push!(types, T)
+                for T in unique(typeof.(col))
+                    if T <: Number
+                        push!(number_types, T)
+                    else
+                        push!(types, T)
+                    end
+                end
+
+                if !isempty(number_types)
+                    push!(types, reduce(Base.promote_typejoin, number_types))
+                end
+
+                if length(types) <= max_types
+                    columns[i] = convert(AbstractVector{Union{types...}}, col)
                 end
             end
+        end
+    else
+        for i in 1:length(columns)
+            col = columns[i]
+            type = eltype(col)
+            if type == Any
+                types = Vector{DataType}()
+                number_types = Vector{DataType}()
 
-            if !isempty(number_types)
-                push!(types, reduce(Base.promote_typejoin, number_types))
-            end
+                for T in unique(typeof.(col))
+                    if T <: Number
+                        push!(number_types, T)
+                    else
+                        push!(types, T)
+                    end
+                end
 
-            if length(types) <= max_types
-                columns[i] = convert(AbstractVector{Union{types...}}, col)
+                if !isempty(number_types)
+                    push!(types, reduce(Base.promote_typejoin, number_types))
+                end
+
+                if length(types) <= max_types
+                    columns[i] = convert(AbstractVector{Union{types...}}, col)
+                end
             end
         end
     end
