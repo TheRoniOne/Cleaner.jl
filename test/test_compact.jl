3d26287084d367a87f6b4f572afdb6def87568fa
TheRoniOne <ronidani99@hotmail.com>
2021-08-30 01:37:45 -0500

    added tests for multithreading

--- test/test_compact.jl
+++ test/test_compact.jl
@@ -1,17 +1,31 @@
 using Test
-using Cleaner: CleanTable, compact_columns, compact_rows, compact_table, size, names
+using Cleaner: CleanTable, compact_columns, compact_rows, compact_rows!, compact_table,
+    cols, size, names
 
 @testset "Compact functions working fine" begin
-    testRM1 = CleanTable([:A, :B, :C], [[missing, missing, missing], [1, missing, 3], ["x", "", "z"]])
-    testRM2 = CleanTable([:A, :B, :C], [["", missing, ""], [1, missing, 3], ["x", missing, "z"]])
+    testRM1 = CleanTable(
+        [:A, :B, :C], [[missing, missing, missing], [1, missing, 3], ["x", "", "z"]]
+    )
+    testRM2 = CleanTable(
+        [:A, :B, :C], [["", missing, ""], [1, missing, 3], ["x", missing, "z"]]
+    )
 
-    @test compact_columns(testRM1) |> size == (3, 2)
-    @test compact_rows(testRM2) |> size == (2, 3)
-    @test compact_table(testRM1) |> size == (3, 2)
+    @test size(compact_columns(testRM1)) == (3, 2)
+    @test size(compact_rows(testRM2)) == (2, 3)
+    @test size(compact_table(testRM1)) == (3, 2)
 
-    @test compact_columns(testRM2, empty_values=[""]) |> size == (3, 2)
-    @test compact_rows(testRM1, empty_values=[""]) |> size == (2, 3)
-    @test compact_table(testRM2, empty_values=[""]) |> size == (2, 2)
+    @test size(compact_columns(testRM2; empty_values=[""])) == (3, 2)
+    @test size(compact_rows(testRM1; empty_values=[""])) == (2, 3)
+    @test size(compact_table(testRM2; empty_values=[""])) == (2, 2)
 
     @test names(compact_columns(testRM1)) == [:B, :C]
+
+    if Threads.nthreads() > 1
+        testRM1 = CleanTable([:A, :B], [collect(1:1_000_000), repeat([missing], 1_000_000)])
+        testRM2 = CleanTable([:A, :B], [collect(1:1_000_000), collect(1:1_000_000)])
+
+        @test size(compact_columns(testRM1)) == (1_000_000, 1)
+        @test size(compact_rows!(testRM2; empty_values=[1, 3, 10])) == (999_997, 2)
+        @test !in(10, cols(testRM2)[1]) && !in(10, cols(testRM2)[2])
+    end
 end
