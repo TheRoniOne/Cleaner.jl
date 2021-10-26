7a4d75ef532a689f42f32131db3ab0bb011fca15
TheRoniOne <ronidani99@hotmail.com>
2021-10-25 21:35:22 -0500

    Removed unused reinfer_schema

--- test/test_row_as_names.jl
+++ test/test_row_as_names.jl
@@ -1,5 +1,5 @@
 using Test
-using Cleaner: CleanTable, row_as_names, reinfer_schema, names, size
+using Cleaner: CleanTable, row_as_names, names, size
 
 @testset "row_as_names is working as expected" begin
     testCT = CleanTable([:A, :B, :C], [[1, 2, "x", 4], [5, 6, "y", 7], ["x", "y", "z", "a"]])
@@ -7,5 +7,4 @@ using Cleaner: CleanTable, row_as_names, reinfer_schema, names, size
     @test names(row_as_names(testCT, 3)) == Symbol[:x, :y, :z]
     @test size(row_as_names(testCT, 3)) == (1, 3)
     @test size(row_as_names(testCT, 3, remove=false)) == (4, 3)
-    
 end
