16230f5718dd1a4791d8a832bde6011f50d3f35a
TheRoniOne <ronidani99@hotmail.com>
2021-10-27 12:39:19 -0500

    Improved tests

--- test/test_delete_const.jl
+++ test/test_delete_const.jl
@@ -2,14 +2,14 @@ using Test
 using Cleaner: CleanTable, delete_const_columns, size, names
 
 @testset "Delete constant columns works as expected" begin
-    testCT = CleanTable([:A, :B, :C], [[1, 1, 1], [4, 5, 6], String["7", "8", "9"]])
+    testCT = CleanTable([:A, :B, :C], [[1, 1, 1], [4, 5, 6], String["2", "2", "2"]])
 
-    @test size(delete_const_columns(testCT)) == (3, 2)
-    @test names(delete_const_columns(testCT)) == [:B, :C]
+    @test size(delete_const_columns(testCT)) == (3, 1)
+    @test names(delete_const_columns(testCT)) == [:B]
     @test delete_const_columns(testCT) isa CleanTable
 
     if Threads.nthreads() > 1
-        testCT2 = CleanTable([:A, :B], [repeat([1], 1_000_000), collect(1:1_000_000)])
+        testCT2 = CleanTable([:A, :B, :C], [repeat([1], 1_000_000), collect(1:1_000_000), repeat([2], 1_000_000)])
         @test size(delete_const_columns(testCT2)) == (1_000_000, 1)
         @test names(delete_const_columns(testCT2)) == [:B]
     end
