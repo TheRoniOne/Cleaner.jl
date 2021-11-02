da25cf341566b8f55baa5a114cdd092cc90e05ec
TheRoniOne <ronidani99@hotmail.com>
2021-11-02 16:07:55 -0500

    Now testing ROT functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,4 @@ end
 @safetestset "test_delete_const.jl" begin include("test_delete_const.jl") end
 @safetestset "test_row_as_names.jl" begin include("test_row_as_names.jl") end
 @safetestset "test_reinfer_schema.jl" begin include("test_reinfer_schema.jl") end
+@safetestset "test_returning_original.jl" begin include("test_returning_original.jl") end
---
+++ test/test_returning_original.jl
@@ -0,0 +1,31 @@
+using Test
+using Cleaner: compact_table_ROT, compact_columns_ROT, compact_rows_ROT, delete_const_columns_ROT, polish_names_ROT, reinfer_schema_ROT, row_as_names_ROT
+using DataFrames: DataFrame
+
+@testset "ROT functions are working as expected" begin
+    testRM1 = DataFrame(A=[missing, missing, missing], B=[1, missing, 3], C=["x", "", "z"])
+
+    @test compact_columns_ROT(testRM1) isa DataFrame
+    @test compact_rows_ROT(testRM1) isa DataFrame
+    @test compact_table_ROT(testRM1) isa DataFrame
+
+    let testDF = DataFrame(A=[1, 1, 1], B=[4, 5, 6], C=String["2", "2", "2"])
+        @test delete_const_columns_ROT(testDF) isa DataFrame
+    end
+
+    let testDF = DataFrame(
+        "  _aName with_loTsOfProblems" => [1, 2, 3],
+        "  _aName with_loTsOfProblems1" => [4, 5, 6],
+        "  _aName with_loTsOfProblems2" => [7, 8, 9],
+    )
+        @test polish_names_ROT(testDF) isa DataFrame
+    end
+
+    let testDF = DataFrame(A=[1, 2, 3], B=Any[4, missing, "z"], C=Any["5", "6", "9"])
+        @test reinfer_schema_ROT(testDF) isa DataFrame
+    end
+
+    let testDF = DataFrame(A=[1, 2, "x", 4], B=[5, 6, "y", 7], C=["x", "y", "z", "a"])
+        @test row_as_names_ROT(testDF, 3) isa DataFrame
+    end
+end
