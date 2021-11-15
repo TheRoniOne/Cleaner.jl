3a3d87926b027eb4fe2ed90af8c928ae714a1e6a
TheRoniOne <ronidani99@hotmail.com>
2021-11-15 14:10:39 -0500

    Added docstrings for add_index and add_index!

--- src/add_index.jl
+++ src/add_index.jl
@@ -1,3 +1,7 @@
+"""
+    add_index!(table::CleanTable)
+Adds in-place a column being the row index for the `CleanTable` table.
+"""
 function add_index!(table::CleanTable)
     columns = cols(table)
     indexes = collect(1:length(columns[1]))
@@ -8,6 +12,11 @@ function add_index!(table::CleanTable)
     return table
 end
 
+"""
+    add_index(table)
+Creates a `CleanTable` with copied columns and adds to it a new column being the row index
+for the table passed.
+"""
 function add_index(table)
     return add_index!(CleanTable(table))
 end
